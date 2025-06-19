d13bc4e6a7af9148a65afda7bb217e3031a4d40a
kasbel-debug <belankasturi10@gmail.com>
2025-06-19 11:24:55 +0530

    Portfolio atlas

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // MongoDB Connection
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/portfolio';
+const MONGODB_URI = process.env.MONGODB_URI;
 
 mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
